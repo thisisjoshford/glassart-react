263d003ddebe1edceb9ec59feb719559560496f9
Josh Ford <thisisjoshford@gmail.com>
2020-02-21 15:21:40 -0800

    css

--- src/GlassItem.js
+++ src/GlassItem.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
+import './GlassItem.css';
 
 export default class GlassItem extends Component {
     render() {
         return (
-            <div>
+            <div id="glassProducts">
                  <h2>{this.props.glass.product_name}</h2>
                  <img src={`https://cryptic-journey-67632.herokuapp.com/assets/${this.props.glass.img_url}`}/>
-                 <li>{this.props.glass.description}</li>
-                 <li>${this.props.glass.price}</li>
-                 <li>{this.props.glass.in_stock.toString()}</li>
-                 <li>{this.props.glass.quantity}</li>
+                 <li id="description">{this.props.glass.description}</li>
+                 <li id="price">${this.props.glass.price}</li>
+                 {/* <li>{this.props.glass.in_stock.toString()}</li>
+                 <li>{this.props.glass.quantity}</li> */}
             </div>
         )
     }
