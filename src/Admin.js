ce9c54be4ed13d6e2ee900bcafef21297a55f3a0
Josh Ford <thisisjoshford@gmail.com>
2020-02-24 12:04:17 -0800

    add handle functions for form data

--- src/Admin.js
+++ src/Admin.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react'
+import { getTypes } from './GlassApi'
 
 export default class Admin extends Component {
+    state = {
+        types: [],
+        in_stock: true,
+        type: 1
+    }
+
+    handleProductName(event){
+        this.setState({ product_name: event.target.value})
+    }
+    handleDescription(event){
+        this.setState({ description: event.target.value})
+    }
+    handlePrice(event){
+        this.setState({ price: Number(event.target.value)})
+
+    }
+    handleImage(event){
+        this.setState({ img_url: event.target.value})
+
+    }
+    handleType(event){
+        this.setState({ type_id: Number(event.target.value)})
+
+    }
+    handleInStock(event){
+        this.setState({ in_stock: Number(event.target.value)})
+    }
+
+    handleQuantity(event){
+        this.setState({ quantity: Number(event.target.value)})
+
+    }
+    handleSubmit(event){
+        event.prevent.default();
+    }
+    async componentDidMount() {
+        const types = await getTypes();
+        this.setState({types: types.body})
+    }
+
     render() {
+
         return (
             <div>
-                <h2>ADMIN PAGE for adding products</h2>
+                <h1>ADMIN</h1>
+                <h2>Enter New Product</h2>
+                <form onSubmit={this.handleSubmit}></form>
             </div>
         )
     }
--- src/GlassApi.js
+++ src/GlassApi.js
@@ -7,3 +7,11 @@ export function getGlassItem(productId){
     const URL = `https://cryptic-journey-67632.herokuapp.com/api/product`
     return request.get(`${URL}/${productId}`)
 }
+
+export function getTypes(){
+    return request.get('https://cryptic-journey-67632.herokuapp.com/api/types')
+}
+
+export function insertNewProduct(product){
+    return request.post('https://cryptic-journey-67632.herokuapp.com/api/products', product)
+}
