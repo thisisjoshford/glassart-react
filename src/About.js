5c95da2fc8cbcb7457a8f9585712ee5b9d486770
Josh Ford <thisisjoshford@gmail.com>
2020-02-24 10:37:03 -0800

    about us data

--- src/About.js
+++ src/About.js
@@ -4,7 +4,10 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                <h2>About us</h2>
+                <h3>Chris Ficht</h3>
+                <p>Chris is a super awesome guy and a killer programmer!</p>
+                <h3>Josh Ford</h3>
+                <p>Josh is pretty ok too...</p>
             </div>
         )
     }
--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Header from './Header.js';
 import Store from './Store.js';
 import About from './About.js';
 import Error from './Error.js';
+import Admin from './Admin.js';
+
 import './App.css';
 
 export default class App extends Component {
@@ -18,13 +20,14 @@ export default class App extends Component {
       <Router>
         <div>
         <Header/>
-        <Link to ="/"><h4>ADMIN</h4></Link>
+        <Link to ="/admin"><h4>ADMIN</h4></Link>
         <Link to ="/"><h4>SHOP</h4></Link>
         <Link to ="/about"><h4>ABOUT</h4></Link>
         <hr></hr>
           <Switch>
             <Route exact path='/' component={Store}/>
             <Route exact path='/about' component={About}/>
+            <Route exact path='/admin' component={Admin}/>
             <Route component={Error}/>
           </Switch>
         </div>
